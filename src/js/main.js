25b42e7116d104799a02d7837616b64e52b03d27
Praba Ponnambalam <praba@codechilli.lk>
2024-03-21 00:56:18 +0530

    🎉 initial commit
    working boilerplate with multi page & bootstrap 5

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import 'the-new-css-reset/css/reset.css';
+
+// Import our custom CSS
+import '../scss/styles.scss';
+
+// Import all of Bootstrap's JS
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap';
---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import eslint from 'vite-plugin-eslint';
+import fs from 'fs';
+
+function generateInputObject() {
+  const srcDir = resolve(__dirname, 'src');
+  const files = fs.readdirSync(srcDir);
+  const input = {};
+
+  files.forEach((file) => {
+    if (file.endsWith('.html')) {
+      const name = file.split('.')[0];
+      input[name] = resolve(srcDir, file);
+    }
+  });
+
+  return input;
+}
+
+export default defineConfig({
+  publicDir: 'public',
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: generateInputObject(),
+    },
+  },
+  server: {
+    port: 8080,
+  },
+  plugins: [
+    eslint({
+      cache: false,
+      fix: true,
+    }),
+    ViteMinifyPlugin({}),
+  ],
+});
