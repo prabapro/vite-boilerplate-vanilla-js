8e2857eb0669f80d17b0aaa078976b40fadf595e
Praba Ponnambalam <praba@codechilli.lk>
2024-03-24 08:07:14 +0530

    💄 add inter font
    added intervariable font as default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./src/*.html', './src/partials/*.html', './src/js/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
